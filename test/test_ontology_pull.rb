2ce877487ef8e95e2a986177884866629a79fd61
Michael Dorf <mdorf@stanford.edu>
2013-09-19 11:56:33 -0700

    improved ontology pull unit tests

--- test/test_ontology_pull.rb
+++ test/test_ontology_pull.rb
@@ -4,9 +4,9 @@ require 'rack'
 class TestOntologyPull < TestCase
 
   def self.before_suite
-    # CREATE FILE OBJECT:
     file = File.new("#{LinkedData.settings.repository_folder}/BROTEST-0/1/BRO_v3.2.owl")
-    #file = File.new("#{LinkedData.settings.repository_folder}/TEST-ONT-0/1/BRO_v3.1.owl")
+    @@port = 4567
+    @@url = "http://localhost:#{@@port}/"
 
     @@thread = Thread.new do
       Rack::Server.start(
@@ -15,7 +15,7 @@ class TestOntologyPull < TestCase
             file.rewind
             [200, {'Content-Type' => 'text/plain'}, [contents]]
           end,
-          Port: 4567
+          Port: port
       )
     end
 
@@ -28,13 +28,7 @@ class TestOntologyPull < TestCase
     end
 
     @@redis.del NcboCron::Models::OntologySubmissionParser::QUEUE_HOLDER
-
     LinkedData::SampleData::Ontology.delete_ontologies_and_submissions
-    @@ont_count, @@acronyms, @@ontologies = LinkedData::SampleData::Ontology.create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: false)
-    @@ontologies[0].bring(:submissions) if @@ontologies[0].bring?(:submissions)
-    @@ontologies[0].submissions[0].bring_remaining()
-    @@ontologies[0].submissions[0].pullLocation = RDF::IRI.new("http://localhost:4567/")
-    @@ontologies[0].submissions[0].save()
   end
 
   def self.after_suite
@@ -45,21 +39,51 @@ class TestOntologyPull < TestCase
 
   def test_remote_ontology_pull()
     pull = NcboCron::Models::OntologyPull.new
+    ontologies = init_ontologies(1)
+    ont = LinkedData::Models::Ontology.find(ontologies[0].id).first
+    ont.bring(:submissions) if ont.bring?(:submissions)
+    assert_equal 1, ont.submissions.length
     pull.do_remote_ontology_pull()
-    ont = LinkedData::Models::Ontology.find(@@ontologies[0].id).first
+
+    # check that the pull creates a new submission when the file has changed
+    ont = LinkedData::Models::Ontology.find(ontologies[0].id).first
     ont.bring(:submissions) if ont.bring?(:submissions)
     assert_equal 2, ont.submissions.length
 
     new_sub = ont.latest_submission(status: :uploaded)
     new_sub.bring_remaining()
     assert_equal 2, new_sub.submissionId
-    assert_equal @@ontologies[0].submissions[0].pullLocation, new_sub.pullLocation
-    assert @@ontologies[0].submissions[0].released < new_sub.released
+    assert_equal ontologies[0].submissions[0].pullLocation, new_sub.pullLocation
+    assert ontologies[0].submissions[0].released < new_sub.released
 
     # test Redis queue
     queue = NcboCron::Models::OntologySubmissionParser.new
     val = @@redis.hget(NcboCron::Models::OntologySubmissionParser::QUEUE_HOLDER, queue.get_prefixed_id(new_sub.id.to_s))
     options = MultiJson.load(val, symbolize_keys: true)
     assert_equal NcboCron::Models::OntologySubmissionParser::ACTIONS, options
+
+    # check that the pull does not create a new submission when the file has not changed
+    ontologies = init_ontologies(2)
+    ont = LinkedData::Models::Ontology.find(ontologies[0].id).first
+    ont.bring(:submissions) if ont.bring?(:submissions)
+    assert_equal 2, ont.submissions.length
+    pull.do_remote_ontology_pull()
+    assert_equal 2, ont.submissions.length
   end
+
+  private
+
+  def init_ontologies(submission_count)
+    ont_count, acronyms, ontologies = LinkedData::SampleData::Ontology.create_ontologies_and_submissions(ont_count: 1, submission_count: submission_count, process_submission: false)
+    ontologies[0].bring(:submissions) if ontologies[0].bring?(:submissions)
+
+    ontologies[0].submissions.each do |sub|
+      sub.bring_remaining()
+      sub.pullLocation = RDF::IRI.new(@@url)
+      sub.save()
+    end
+
+    return ontologies
+  end
+
 end

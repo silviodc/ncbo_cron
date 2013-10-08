c78cd3909302d0d6a70dad183bcd894c09fb3e61
msalvadores <msalvadores@gmail.com>
2013-10-08 13:02:09 -0700

    script to queue jobs in batch

---
+++ lib/ncbo_cron/enqueue_batch.rb
@@ -0,0 +1,27 @@
+require_relative 'ontology_submission_parser'
+
+def get_obo_submissions
+  subs = []
+  LinkedData::Models::Ontology.where.include(:acronym, :summaryOnly).all.each do |ont|
+    if !ont.summaryOnly
+      sub = ont.latest_submission(status: :any)
+      if sub
+        sub.bring(:hasOntologyLanguage)
+        if sub.hasOntologyLanguage.obo?
+          subs << sub
+        end
+      else
+        puts "OBO ontology with no submissions #{ont.id.to_s}"
+      end
+    end
+  end
+  return subs
+end
+
+submission_queue = NcboCron::Models::OntologySubmissionParser.new
+
+submissions = get_obo_submissions
+submissions.each do |s|
+  submission_queue.queue_submission(sub, {all: true})
+end
+puts "Added #{submissions.length} to the queue."

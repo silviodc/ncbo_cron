caeab274a1804cebadc1d03797577b0f0e505d3e
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2014-01-24 17:17:39 -0800

    Script for routine diagnosis of ontology processing problems.

--- bin/ncbo_ontology_diagnostics.sh
+++ bin/ncbo_ontology_diagnostics.sh
@@ -10,17 +10,26 @@ echo "Inspecting submissionStatus and metrics for all ontologies."
 ./bin/ncbo_ontology_inspector -p submissionStatus,metrics > logs/submission_status.log
 
 # Filter the output log to remove summaryOnly ontologies.
-grep -v 'summaryOnly' logs/submission_status.log > logs/submission_status_notSummaryOnly.log
+grep -v -F 'summaryOnly' logs/submission_status.log > logs/submission_status_notSummaryOnly.log
 
 echo
 echo "Ontologies failing to parse, without RDF data:"
-grep 'ERROR_RDF' logs/submission_status_notSummaryOnly.log
+grep -F 'ERROR_RDF' logs/submission_status_notSummaryOnly.log
+
+# Filter the output log to remove RDF errors from the ontologies with submissions.
+grep -v -F 'ERROR_RDF' logs/submission_status_notSummaryOnly.log > logs/submission_status_withRDF.log
 
 echo
-echo "Ontologies with RDF data, without metrics:"
-grep -v 'ERROR_RDF' logs/submission_status_notSummaryOnly.log | grep -v 'METRICS'
+echo "Ontologies with RDF data, without METRICS:"
+grep -v -F 'METRICS' logs/submission_status_withRDF.log
 
 echo
 echo "Ontologies with RDF data, without SOLR data:"
-grep -v 'ERROR_RDF' logs/submission_status_notSummaryOnly.log | grep -v 'METRICS'
+grep -F 'INDEXCOUNT:0' logs/submission_status_withRDF.log
+grep -F 'INDEXCOUNT_MISSING' logs/submission_status_withRDF.log
+grep -F 'INDEXCOUNT_ERROR' logs/submission_status_withRDF.log
 
+echo
+echo "Ontologies with RDF data, without ANNOTATOR data:"
+grep -F 'ANNOTATOR_MISSING' logs/submission_status_withRDF.log
+grep -F 'ANNOTATOR_ERROR' logs/submission_status_withRDF.log

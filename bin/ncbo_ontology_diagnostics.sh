9033915bc6c3ec4204d7070f2f6a319eea7afcb6
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2014-01-30 15:38:37 -0800

    General purpose CLI for ontology inspections - work in progress.

--- bin/ncbo_ontology_diagnostics.sh
+++ bin/ncbo_ontology_diagnostics.sh
@@ -5,25 +5,33 @@
 # 3) Ontologies with no classes indexed in SOLR
 # 4) Ontologies that are not in the annotator cache.
 
+
+REFORMAT_LINES='s/;;/\n\t/g'
+
+
 echo "Inspecting submissionStatus and metrics for all ontologies."
 ./bin/ncbo_ontology_inspector -p submissionStatus,metrics > logs/submission_status.log
 
 echo
 echo '*********************************************************************************************'
 echo 'System config:'
-grep -E '(.*) >> Using' logs/submission_status.log
+CONFIG_PATTERN='(.*) >> Using'
+grep -E $CONFIG_PATTERN logs/submission_status.log
+grep -v -E $CONFIG_PATTERN logs/submission_status.log > logs/tmp.log
+mv logs/tmp.log logs/submission_status.log
 
 echo
 echo '*********************************************************************************************'
 echo 'Filtering log to remove summaryOnly ontologies.'
 echo
-grep -v -F 'summaryOnly' logs/submission_status.log | grep -v -E '(.*) >> Using' > logs/submission_status_notSummaryOnly.log
+grep -v -F 'summaryOnly' logs/submission_status.log > logs/tmp.log
+sed -e 's/ontology=found;;//' logs/tmp.log > logs/submission_status_notSummaryOnly.log
 
 echo
 echo '*********************************************************************************************'
 echo 'Ontologies missing a latest submission:'
 echo
-grep -F 'submissionId=ERROR' logs/submission_status_notSummaryOnly.log
+grep -F 'submissionId=ERROR' logs/submission_status_notSummaryOnly.log | sed -e $REFORMAT_LINES
 # cleanup the submission log
 grep -v -F 'submissionId=ERROR' logs/submission_status_notSummaryOnly.log > logs/submission_status_hasSubmission.log
 
@@ -31,7 +39,7 @@ echo
 echo '*********************************************************************************************'
 echo "Ontologies failing to parse, without RDF data ('ERROR_RDF','ERROR_RDF_LABELS'):"
 echo
-grep -F 'ERROR_RDF' logs/submission_status_hasSubmission.log
+grep -F 'ERROR_RDF' logs/submission_status_hasSubmission.log | sed -e $REFORMAT_LINES
 # Filter the output log to remove RDF errors from the ontologies with submissions.
 grep -v -F 'ERROR_RDF' logs/submission_status_hasSubmission.log > logs/submission_status_withRDF.log
 
@@ -39,23 +47,23 @@ echo
 echo '*********************************************************************************************'
 echo 'Ontologies with RDF data, without METRICS:'
 echo
-grep -v -F 'METRICS' logs/submission_status_withRDF.log
-grep -F 'METRICS_MISSING' logs/submission_status_withRDF.log
-grep -F 'classes:0' logs/submission_status_withRDF.log
-grep -F 'maxDepth:0' logs/submission_status_withRDF.log
+grep -v -F 'METRICS' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'METRICS_MISSING' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'classes:0' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'maxDepth:0' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
 
 echo
 echo '*********************************************************************************************'
 echo "Ontologies with RDF data, without SOLR data:"
 echo
-grep -F 'INDEXCOUNT:0' logs/submission_status_withRDF.log
-grep -F 'INDEXCOUNT_MISSING' logs/submission_status_withRDF.log
-grep -F 'INDEXCOUNT_ERROR' logs/submission_status_withRDF.log
+grep -F 'INDEXCOUNT:0' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'INDEXCOUNT_MISSING' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'INDEXCOUNT_ERROR' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
 
 echo
 echo '*********************************************************************************************'
 echo "Ontologies with RDF data, without ANNOTATOR data:"
 echo
-grep -F 'ANNOTATOR_MISSING' logs/submission_status_withRDF.log
-grep -F 'ANNOTATOR_ERROR' logs/submission_status_withRDF.log
+grep -F 'ANNOTATOR_MISSING' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
+grep -F 'ANNOTATOR_ERROR' logs/submission_status_withRDF.log | sed -e $REFORMAT_LINES
 

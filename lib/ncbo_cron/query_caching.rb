ed458ff88d70f69246a68ae07ef32ade5f473bea
msalvadores <msalvadores@gmail.com>
2014-09-17 19:00:05 -0700

    NCBO-994 query counts moved to LD

--- lib/ncbo_cron/query_caching.rb
+++ lib/ncbo_cron/query_caching.rb
@@ -35,74 +35,10 @@ module NcboCron
       def run
         @logger.info("running counts per ontology")
         @logger.flush()
-        new_counts = LinkedData::Mappings.mapping_counts(
-                                            enable_debug=true,logger=@logger,
-                                            reload_cache=true)
-        persistent_counts = {}
-        LinkedData::Models::MappingCount.where(pair_count: false)
-        .include(:all)
-        .all
-        .each do |m|
-          persistent_counts[m.ontologies.first] = m
-        end
-        
-        new_counts.each do |acr|
-          new_count = counts[acr]
-          if persistent_counts.include?(acr)
-            inst = model_count[acr]
-            if new_count != inst.count
-              inst.count = new_count
-              inst.save
-            end
-          else
-            m = LinkedData::Models::MappingCount.new
-            m.ontologies = [acr]
-            m.pair_count = false
-            m.count = new_count
-            m.save
-          end
-        end
+        LinkedData::Mappings.create_mapping_counts(@logger)
+
         iterations = 0
-        @logger.info("running first page classes and mappings of each ontology")
-        @logger.flush()
-
-
         retrieve_latest_submissions.each do |acr,sub|
-          @logger.info("running mapping counts #{sub.id.to_s}")
-          @logger.flush()
-
-          new_counts = LinkedData::Mappings
-                    .mapping_ontologies_count(sub,nil,reload_cache=true)
-          persistent_counts = {}
-          LinkedData::Models::MappingCount.where(pair_count: false,
-                                                 ontologies: [acr])
-          .include(:all)
-          .all
-          .each do |m|
-            other = m.ontologies.first
-            if other == acr
-              other = m.ontologies[1]
-            end
-            persistent_counts[other] = m
-          end
-          
-          new_counts.each do |other|
-            new_count = counts[other]
-            if persistent_counts.include?(other)
-              inst = model_count[other]
-              if new_count != inst.count
-                inst.count = new_count
-                inst.save
-              else
-                m = LinkedData::Models::MappingCount.new
-                m.count = new_count
-                m.ontologies = [acr,other]
-                m.pair_count = true
-                m.save
-              end
-            end
-          end
-
           @logger.info("running first page mappings #{sub.id.to_s}")
           @logger.flush()
           mappings = LinkedData::Mappings

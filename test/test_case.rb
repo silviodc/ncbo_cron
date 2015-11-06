e92ed6b64221f4d675c9cb082734a6de9ae9dda6
mdorf <mdorf@stanford.edu>
2015-11-06 13:11:30 -0800

    fixed ncbo_cron unit tests

--- test/test_case.rb
+++ test/test_case.rb
@@ -50,6 +50,7 @@ class CronUnit < MiniTest::Unit
       Goo.sparql_update_client.update("DELETE {?s ?p ?o } WHERE { ?s ?p ?o }")
       LinkedData::Models::SubmissionStatus.init_enum
       LinkedData::Models::OntologyFormat.init_enum
+      LinkedData::Models::OntologyType.init_enum
       LinkedData::Models::Users::Role.init_enum
       LinkedData::Models::Users::NotificationType.init_enum
     else
